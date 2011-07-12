a7bcfae553b28e6e81b7c8a2e743727e4c2304a8
Kang Chen <nagareboshi@gmail.com>
2011-07-12 01:14:46 -0700

    fixed corner ad not showing up issue

--- js/home_optimized.js
+++ js/home_optimized.js
@@ -1,5 +1,5 @@
 // home.js
-// document.write('');
+document.write('');
 e9 = new Object();
 e9.size = "160x600";
 //-->
@@ -14,110 +14,115 @@ if (e9.displayAdFlag == true) {
 }
 
 // feed.js
-$(function() {
-    $('#mainContent div:not(:first)').hide();
+if(typeof jQuery != 'undefined') {
+  $(function() {
+      $('#mainContent div:not(:first)').hide();
 
-    $('ul#tabnav li a').click(function() {
-        $('ul#tabnav li a').removeClass('selected');
-        $(this).addClass('selected');
+      $('ul#tabnav li a').click(function() {
+          $('ul#tabnav li a').removeClass('selected');
+          $(this).addClass('selected');
 
-        var href = $(this).attr('href');
-        var split = href.split('#');
+          var href = $(this).attr('href');
+          var split = href.split('#');
 
-        $('#mainContent div').hide();
-        $('#mainContent div#' + split[1]).fadeIn();
+          $('#mainContent div').hide();
+          $('#mainContent div#' + split[1]).fadeIn();
 
-        return false;
-    });
-});
+          return false;
+      });
+  });
+}
+
 
 // slideshow.js
-var $$ = $.fn;
+if(typeof jQuery != 'undefined') {
+  var $$ = $.fn;
 
-$$.extend({
-  SplitID : function()
-  {
-    return this.attr('id').split('-').pop();
-  },
-
-  Slideshow : {
-    Ready : function()
+  $$.extend({
+    SplitID : function()
     {
-      $('div.tmpSlideshowControl')
-        .hover(
-          function() {
-            $(this).addClass('tmpSlideshowControlOn');
-          },
-          function() {
-            $(this).removeClass('tmpSlideshowControlOn');
-          }
-        )
-        .click(
-          function() {
-            $$.Slideshow.Interrupted = true;
+      return this.attr('id').split('-').pop();
+    },
 
-            $('div.tmpSlide').hide();
-            $('div.tmpSlideshowControl').removeClass('tmpSlideshowControlActive');
+    Slideshow : {
+      Ready : function()
+      {
+        $('div.tmpSlideshowControl')
+          .hover(
+            function() {
+              $(this).addClass('tmpSlideshowControlOn');
+            },
+            function() {
+              $(this).removeClass('tmpSlideshowControlOn');
+            }
+          )
+          .click(
+            function() {
+              $$.Slideshow.Interrupted = true;
 
-            $('div#tmpSlide-' + $(this).SplitID()).show()
-            $(this).addClass('tmpSlideshowControlActive');
+              $('div.tmpSlide').hide();
+              $('div.tmpSlideshowControl').removeClass('tmpSlideshowControlActive');
+
+              $('div#tmpSlide-' + $(this).SplitID()).show()
+              $(this).addClass('tmpSlideshowControlActive');
             
-            $$.Slideshow.Counter = parseInt($(this).SplitID());
-            setTimeout('$$.Slideshow.Resume();', 5000); // Resume after 5 seconds  
-          }
-        );
+              $$.Slideshow.Counter = parseInt($(this).SplitID());
+              setTimeout('$$.Slideshow.Resume();', 5000); // Resume after 5 seconds  
+            }
+          );
 
-      this.Counter = 1;
-      this.Interrupted = false;
+        this.Counter = 1;
+        this.Interrupted = false;
 
-      this.Transition();
-    },
+        this.Transition();
+      },
 
-    Resume : function()
-    {
-      this.Interrupted = false;  
-      this.Transition();      
-    },
+      Resume : function()
+      {
+        this.Interrupted = false;  
+        this.Transition();      
+      },
 
-    Transition : function()
-    {
-      if (this.Interrupted) {
-        return;
-      }
+      Transition : function()
+      {
+        if (this.Interrupted) {
+          return;
+        }
 
-      this.Last = this.Counter - 1;
+        this.Last = this.Counter - 1;
 
-      if (this.Last < 1) {
-        this.Last = 5;
-      }
+        if (this.Last < 1) {
+          this.Last = 5;
+        }
 
-      $('div#tmpSlide-' + this.Last).fadeOut(1000);
+        $('div#tmpSlide-' + this.Last).fadeOut(1000);
         
-      $('div#tmpSlide-' + $$.Slideshow.Counter).fadeIn(
-        1000,
-        function() {
-          $('div#tmpSlideshowControl-' + $$.Slideshow.Last).removeClass('tmpSlideshowControlActive');
-          $('div#tmpSlideshowControl-' + $$.Slideshow.Counter).addClass('tmpSlideshowControlActive');
+        $('div#tmpSlide-' + $$.Slideshow.Counter).fadeIn(
+          1000,
+          function() {
+            $('div#tmpSlideshowControl-' + $$.Slideshow.Last).removeClass('tmpSlideshowControlActive');
+            $('div#tmpSlideshowControl-' + $$.Slideshow.Counter).addClass('tmpSlideshowControlActive');
           
 
-          $$.Slideshow.Counter++;
+            $$.Slideshow.Counter++;
 
-          if ($$.Slideshow.Counter > 5) {
-            $$.Slideshow.Counter = 1;
+            if ($$.Slideshow.Counter > 5) {
+              $$.Slideshow.Counter = 1;
+            }
+
+            setTimeout('$$.Slideshow.Transition();', 5000);
           }
-
-          setTimeout('$$.Slideshow.Transition();', 5000);
-        }
-      );
+        );
+      }
     }
-  }
-});
+  });
 
-$(document).ready(
-  function() {
-    $$.Slideshow.Ready();
-  }
-);
+  $(document).ready(
+    function() {
+      $$.Slideshow.Ready();
+    }
+  );
+};
 
 
 // flashwrite_1_2.js
