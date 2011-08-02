82934b4eecab5d1fe9be2af03de94517cd01adbf
jiajih <chang.jiajih@gmail.com>
2011-08-02 08:41:55 -0700

    adding domain to gn_country cookie

--- js/geo.php
+++ js/geo.php
@@ -1,1 +1,1 @@
-gn_country='US';ip='209.129.244.250';exdate=new Date();exdate.setDate(exdate.getDate()+ 7);document.cookie="gn_country=US; expires=" + exdate.toGMTString() + "; path=/";
+gn_country='US';ip='209.129.244.250';exdate=new Date();exdate.setDate(exdate.getDate()+ 7);document.cookie="gn_country=US; expires=" + exdate.toGMTString() + "; domain=www.webperflab.com; path=/";
