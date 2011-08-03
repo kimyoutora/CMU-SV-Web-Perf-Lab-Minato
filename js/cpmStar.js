8351d0a31492382274e0c388e3d60563acdd142a
jiajih <chang.jiajih@gmail.com>
2011-08-02 22:18:05 -0700

    updating cdn paths

--- js/cpmStar.js
+++ js/cpmStar.js
@@ -9,13 +9,13 @@ cpmStarAdInfo['desc'] = 'Journey to become a Ninja of Legend. Set in the world o
 cpmStarAdInfo['link'] = 'http://server.cpmstar.com/click.aspx?poolid=12498&subpoolid=0&campaignid=22709&creativeid=53065&url=';
 
 var cpmStarImgs = [];
-if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/PockieNinja2_60x90.jpg',width:60,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/PockieNinja2_90x60.jpg',width:90,height:60});
-if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/PockieNinja2_90x90.jpg',width:90,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_140x105.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/PockieNinja2_140x105.jpg',width:140,height:105});
-if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_140x140.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/PockieNinja2_140x140.jpg',width:140,height:140});
-if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_180x100.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/PockieNinja2_180x100.jpg',width:180,height:100});
-if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_180x250.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/PockieNinja2_180x250.jpg',width:180,height:250});
+if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/PockieNinja2_60x90.jpg',width:60,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/PockieNinja2_90x60.jpg',width:90,height:60});
+if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/PockieNinja2_90x90.jpg',width:90,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_140x105.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/PockieNinja2_140x105.jpg',width:140,height:105});
+if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_140x140.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/PockieNinja2_140x140.jpg',width:140,height:140});
+if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_180x100.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/PockieNinja2_180x100.jpg',width:180,height:100});
+if ('http://cdn3.cpmstar.com/cached/creatives/53065/PockieNinja2_180x250.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/PockieNinja2_180x250.jpg',width:180,height:250});
 cpmStarAdInfo.imgs = cpmStarImgs;
 
 cpmStarMultiAdInfo.push(cpmStarAdInfo);
@@ -30,11 +30,11 @@ cpmStarAdInfo['desc'] = 'If you can imagine it, you can build it! Create games a
 cpmStarAdInfo['link'] = 'http://server.cpmstar.com/click.aspx?poolid=12498&subpoolid=0&campaignid=4018&creativeid=25472&url=';
 
 var cpmStarImgs = [];
-if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_roblox60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/7093_roblox60x90.jpg',width:60,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_roblox90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/7093_roblox90x60.jpg',width:90,height:60});
-if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_roblox90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/7093_roblox90x90.jpg',width:90,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_Roblox_140x105.gif' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/7093_Roblox_140x105.gif',width:140,height:105});
-if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_Roblox_140x140.gif' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/7093_Roblox_140x140.gif',width:140,height:140});
+if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_roblox60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/7093_roblox60x90.jpg',width:60,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_roblox90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/7093_roblox90x60.jpg',width:90,height:60});
+if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_roblox90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/7093_roblox90x90.jpg',width:90,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_Roblox_140x105.gif' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/7093_Roblox_140x105.gif',width:140,height:105});
+if ('http://cdn3.cpmstar.com/cached/creatives/25472/7093_Roblox_140x140.gif' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/7093_Roblox_140x140.gif',width:140,height:140});
 if ('' != '') cpmStarImgs.push({url:'',width:180,height:100});
 if ('' != '') cpmStarImgs.push({url:'',width:180,height:250});
 cpmStarAdInfo.imgs = cpmStarImgs;
@@ -51,13 +51,13 @@ cpmStarAdInfo['desc'] = 'World of Tanks is a team-based massively multiplayer on
 cpmStarAdInfo['link'] = 'http://server.cpmstar.com/click.aspx?poolid=12498&subpoolid=0&campaignid=22773&creativeid=53180&url=';
 
 var cpmStarImgs = [];
-if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/WorldofTanks_60x90.jpg',width:60,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/WorldofTanks_90x60.jpg',width:90,height:60});
-if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/WorldofTanks_90x90.jpg',width:90,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_140x105.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/WorldofTanks_140x105.jpg',width:140,height:105});
-if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_140x140.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/WorldofTanks_140x140.jpg',width:140,height:140});
-if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_180x100.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/WorldofTanks_180x100.jpg',width:180,height:100});
-if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_180x250.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/WorldofTanks_180x250.jpg',width:180,height:250});
+if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/WorldofTanks_60x90.jpg',width:60,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/WorldofTanks_90x60.jpg',width:90,height:60});
+if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/WorldofTanks_90x90.jpg',width:90,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_140x105.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/WorldofTanks_140x105.jpg',width:140,height:105});
+if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_140x140.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/WorldofTanks_140x140.jpg',width:140,height:140});
+if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_180x100.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/WorldofTanks_180x100.jpg',width:180,height:100});
+if ('http://cdn3.cpmstar.com/cached/creatives/53180/WorldofTanks_180x250.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/WorldofTanks_180x250.jpg',width:180,height:250});
 cpmStarAdInfo.imgs = cpmStarImgs;
 
 cpmStarMultiAdInfo.push(cpmStarAdInfo);
@@ -72,13 +72,13 @@ cpmStarAdInfo['desc'] = 'Choose between Humans or Cylons in this new game with s
 cpmStarAdInfo['link'] = 'http://server.cpmstar.com/click.aspx?poolid=12498&subpoolid=0&campaignid=24880&creativeid=58513&url=';
 
 var cpmStarImgs = [];
-if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/BGO60x90.jpg',width:60,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/BGO90x60.jpg',width:90,height:60});
-if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/BGO90x90.jpg',width:90,height:90});
-if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO140x105.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/BGO140x105.jpg',width:140,height:105});
-if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO140x140.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/BGO140x140.jpg',width:140,height:140});
-if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO180x100.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/BGO180x100.jpg',width:180,height:100});
-if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO180x250.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17/images/BGO180x250.jpg',width:180,height:250});
+if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO60x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/BGO60x90.jpg',width:60,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO90x60.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/BGO90x60.jpg',width:90,height:60});
+if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO90x90.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/BGO90x90.jpg',width:90,height:90});
+if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO140x105.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/BGO140x105.jpg',width:140,height:105});
+if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO140x140.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/BGO140x140.jpg',width:140,height:140});
+if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO180x100.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/BGO180x100.jpg',width:180,height:100});
+if ('http://cdn3.cpmstar.com/cached/creatives/58513/BGO180x250.jpg' != '') cpmStarImgs.push({url:'http://cdn.webperflab.com/minato/1_2_3_4_5_6_8_10_12_13_15_17_22/images/BGO180x250.jpg',width:180,height:250});
 cpmStarAdInfo.imgs = cpmStarImgs;
 
 cpmStarMultiAdInfo.push(cpmStarAdInfo);
