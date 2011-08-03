4386bba9082da6bd10fb02ab776a6c3948b691e3
jiajih <chang.jiajih@gmail.com>
2011-08-02 22:09:46 -0700

    adding async, tentative change to home_optimized.js pending error checking

--- js/home_optimized.js
+++ js/home_optimized.js
@@ -1,11 +1,7 @@
 // home.js
-document.write('');
 e9 = new Object();
 e9.size = "160x600";
 //-->
-if(typeof showDisplayAd != 'undefined' && showDisplayAd != null && showDisplayAd == true) {
-  document.write('\n<script type=\"text/javascript\" src=\"../js/tags.js\"><\/script>');document.write('\n');
-}
 
 // displayAd.js
 var e9;
